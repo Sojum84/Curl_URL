1ffe326b2cab41f4e94207f2663a01da50508267
Sojum84 <92978445+Sojum84@users.noreply.github.com>
2021-10-26 11:44:57 +0300

    Add files via upload

---
+++ Includes/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define ('SITE_NAME',"Cut you URL");
+define ('HOST',"http://" . $_SERVER['HTTP_HOST']);
+
+//db settings
+define ('DB_HOST','127.0.0.1');
+define ('DB_NAME','cur_url');
+define ('DB_USER','root');
+define ('DB_PASS','usbw');
+
+?>
